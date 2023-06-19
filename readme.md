b5c1ce0b003f6f4f36be800de0767e792473f6af
Ryan Adhitama <ryanadhitama2@gmail.com>
2023-06-19 09:52:03 +0800

    first echo route

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Echo Basic
+
+- [x] First route
---
+++ server.go
@@ -0,0 +1,14 @@
+package echo_basic
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
